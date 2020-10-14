146722a9a4835b6968fcda94320ee79216fb5e7d
thulijay <thulijay79@gmail.com>
2020-10-14 11:59:25 +0200

    updated database name..

--- iden.sql
+++ iden.sql
@@ -1,4 +1,4 @@
-create table identity(
+create table users(
 	id serial not null primary key,
 	user_name text,
 	user_count int
--- test/greetings-test.js
+++ test/greetings-test.js
@@ -3,9 +3,9 @@ const greetings = require('../greetings-factory');
 const pg =require('pg');
 const Pool = pg.Pool;
 
-const INSERT_QUERY = 'insert into identity(user_name, user_count) values ($1, $2)';
+const INSERT_QUERY = 'insert into users(user_name, user_count) values ($1, $2)';
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:pg123@localhost:5432/identity';
+const connectionString = process.env.DATABASE_URL || 'postgresql://melissa:pg123@localhost:5432/users';
 
 const pool = new pg.Pool({
     connectionString
@@ -14,7 +14,7 @@ const pool = new pg.Pool({
 
 describe('Greetings-Webapp Tests', function(){
     beforeEach(async function(){
-        await pool.query('delete from identity;');
+        await pool.query('delete from users;');
     });
 
     it('should be able to add a name', async function(){
@@ -22,7 +22,7 @@ describe('Greetings-Webapp Tests', function(){
         // await pool.query(INSERT_QUERY, ['james', 1]);
         // await pool.query(INSERT_QUERY, ['champ', 1]);
 
-        const returnDb = await pool.query("select count(*) from identity");
+        const returnDb = await pool.query("select count(*) from users");
     
         assert.equal(1, returnDb.rows[0].count);
     });
@@ -32,7 +32,7 @@ describe('Greetings-Webapp Tests', function(){
         await pool.query(INSERT_QUERY, ['rein', 1]);
         await pool.query(INSERT_QUERY, ['curtley', 1])
 
-        const returnDb = await pool.query("select count(*) from identity");
+        const returnDb = await pool.query("select count(*) from users");
 
         assert.equal(3, returnDb.rows[0].count);
     });
@@ -41,7 +41,7 @@ describe('Greetings-Webapp Tests', function(){
         await pool.query(INSERT_QUERY, ['rebecca', 1]);
         await pool.query(INSERT_QUERY, ['rebecca', 1]);
 
-        const returnDb = await pool.query('select count (*) from identity');
+        const returnDb = await pool.query('select count (*) from users');
 
         assert.equal(2, returnDb.rows[0].count);
     });
@@ -51,7 +51,7 @@ describe('Greetings-Webapp Tests', function(){
     describe('More Tests', function(){
 
         beforeEach(async function(){
-            await pool.query('delete from identity;');
+            await pool.query('delete from users;');
         });
 
     it('should be able to reset data', async function(){
@@ -60,7 +60,7 @@ describe('Greetings-Webapp Tests', function(){
         await pool.query(INSERT_QUERY, ['thembi', 1]);
         await pool.query(INSERT_QUERY, ['troy', 1]);
 
-        const returnDb = await pool.query('select count (*) from identity');
+        const returnDb = await pool.query('select count (*) from users');
 
         assert.deepEqual(4, returnDb.rows[0].count, []);
     });
@@ -68,19 +68,19 @@ describe('Greetings-Webapp Tests', function(){
     it('should not return an empty entry', async function(){
         await pool.query(INSERT_QUERY, ['', 0])
 
-        const returnDb = await pool.query('select count (*) from identity');
+        const returnDb = await pool.query('select count (*) from users');
 
         assert.equal(undefined, returnDb.rows.count)
     });
 
     it('should be able to greet user in English', async function(){
-        const INSERT_QUERY = 'insert into identity(user_name) values ($1)';
+        const INSERT_QUERY = 'insert into users(user_name) values ($1)';
 
         let language = "English";
 
         await pool.query(INSERT_QUERY, ['kim'], language);
 
-        const returnDb = await pool.query('select count (*) from identity');
+        const returnDb = await pool.query('select count (*) from users');
 
         assert.equal(0, returnDb.rows[0].count);
     })
